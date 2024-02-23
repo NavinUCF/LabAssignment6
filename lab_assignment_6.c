0480a8efbe764845df1e9b2f8d666d1aa4105b0d
Navin Singh <scratchtiger11@gmail.com>
2024-02-23 16:20:51 -0500

    Finished Lab 6

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,8 +1,33 @@
+//Navin Singh COP 3502C
+//Lab 6 2/23/2024
 #include <stdio.h>
-
+#include <stdlib.h>
+//use binary search method this time recursively
 int search(int numbers[], int low, int high, int value) 
 {
+	//if the recursion keeps happening to where the low is bigger than high, means the value is not in the array
+	if(low<=high){
+		//calculate the mid index to see if the value is at mid index
+		int mid;
+		mid = (low +high)/2;
+		//if it is, simply return mid
+		if(numbers[mid]==value)
+			return mid;
+		//else if the value is less than mid, it means value is in the index below mid, so search again with same low and mid-1 is new high
+		else if(value<numbers[mid]){
+			return search(numbers, low, mid-1, value);
+		}
+		//else if the value is greater than mid, value is in index above mid, so search with mid+1 is new low and high is kept the same
+		else if(value>numbers[mid]){
+			return search(numbers, mid+1, high, value);
+		}
+	}
+	//base case 
 	return -1;
+	
+
+
+	
 }
 
 void printArray(int numbers[], int sz)
